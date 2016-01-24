baa5f833c7c86dbe99f1355288fdfa51eb1eff79
vijayan <viji69.apgs@gmail.com>
2016-01-25 01:14:37 +0530

    Latest update 25-1-2016

--- wp-content/themes/webhooo/header.php
+++ wp-content/themes/webhooo/header.php
@@ -7,7 +7,10 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<link href='https://fonts.googleapis.com/css?family=Orbitron:500,400' rel='stylesheet' type='text/css'>
+
+
+<link href='https://fonts.googleapis.com/css?family=Oswald:700,400' rel='stylesheet' type='text/css'>
+
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
